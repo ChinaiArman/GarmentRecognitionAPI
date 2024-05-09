acf6c47f915491a008e5e7c663bd05dcce9ff430
Collin <omiyage4949@gmail.com>
2024-05-09 11:26:06 -0700

    Add more CSV data from api sources

--- server/data-source/data_aggregation.py
+++ server/data-source/data_aggregation.py
@@ -11,7 +11,7 @@ import json
 HTTP_SUCCESS_CODE = 200
 
 
-async def get_asos_data(page, session):
+async def get_asos_data(session, page, category):
     """
     Gets JSON data from api source with single HTTP request.
     """
@@ -45,7 +45,7 @@ async def get_asos_data(page, session):
         return None
 
 
-async def get_hm_data(page, session):
+async def get_hm_data(session, page, category):
     """
     Gets JSON data from api source with single HTTP request.
     """
@@ -55,8 +55,7 @@ async def get_hm_data(page, session):
         "lang": "en",
         "currentpage": page,
         "pagesize": "30",
-        "categories": "men_all",
-        "concepts": "H&M MAN",
+        "categories": category,
     }
     api_headers = {
         "X-RapidAPI-Key": os.getenv("RAPID_API_KEY", ""),
@@ -83,7 +82,10 @@ def write_asos_data(responses):
     df_list = []
     for response in responses:
         df = pd.json_normalize(response["products"])
-        df["imageUrl"] = df["imageUrl"].apply(lambda x: f"https://{x}")
+        try:
+            df["imageUrl"] = df["imageUrl"].apply(lambda x: f"https://{x}")
+        except KeyError:
+            continue
         df = df[["id", "name", "colour", "brandName", "imageUrl"]]
         df_list.append(df)
 
@@ -98,7 +100,10 @@ def write_hm_data(responses):
     df_list = []
     for response in responses:
         df = pd.json_normalize(response["results"])
-        df["images"] = df["images"].apply(lambda x: x[0]["baseUrl"])
+        try:
+            df["images"] = df["images"].apply(lambda x: x[0]["baseUrl"])
+        except KeyError:
+            continue
         df = df[["code", "name", "defaultArticle.color.text", "images"]]
         df_list.append(df)
 
@@ -106,14 +111,20 @@ def write_hm_data(responses):
     df_merged.to_csv("server/data-source/data-files/hm.csv", index=False)
 
 
-async def process_requests(pages, async_get_request, write_to_csv):
+async def process_requests(
+    async_get_request, write_to_csv, pages=[""], categories=[""]
+):
     """
     Executes multiple asynchronous HTTP requests.
     """
     # Use following line for handling rate limiting on certain APIs by setting connection limit
     connector = aiohttp.TCPConnector(limit_per_host=1)
     async with aiohttp.ClientSession(connector=connector) as session:
-        async_coroutines = [async_get_request(page, session) for page in pages]
+        async_coroutines = [
+            async_get_request(session, page, category)
+            for category in categories
+            for page in pages
+        ]
 
         responses = await asyncio.gather(*async_coroutines)
         responses_filtered = [
@@ -129,11 +140,12 @@ async def main():
     """
     # Get data from ASOS
     asos_offset = [str(num) for num in range(0, 48 * 8, 48)]
-    await process_requests(asos_offset, get_asos_data, write_asos_data)
+    await process_requests(get_asos_data, write_asos_data, asos_offset)
 
     # Get data from H&M
-    hm_pages = [str(num) for num in range(1, 6)]
-    await process_requests(hm_pages, get_hm_data, write_hm_data)
+    hm_pages = [str(num) for num in range(1, 7)]
+    hm_categories = ["men_all", "ladies_all"]
+    await process_requests(get_hm_data, write_hm_data, hm_pages, hm_categories)
 
 
 if __name__ == "__main__":
