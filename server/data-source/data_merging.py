217c69ef126a11a4df7e2e1df4656154c7014610
Ehsan138 <67409692+Ehsan138@users.noreply.github.com>
2024-05-09 17:58:10 -0700

    Add documentation for dataset merging script with requirements

--- server/data-source/data_merging.py
+++ server/data-source/data_merging.py
@@ -1,8 +1,41 @@
+"""
+Author: ``@Ehsan138``
+Version: 1.0.0
+
+Description:
+Merges two datasets: one containing image URLs and another containing styles information.
+The function merges these datasets based on the image filenames and style IDs after processing them for compatibility.
+The merged dataset is then saved as a new CSV file.
+
+Requirements:
+This script requires the installation of the pandas library.
+Ensure pandas is installed using the following command: ``pip install pandas``
+
+Usage:
+To execute this module, run the following command:
+    ``python merge_datasets_script.py``
+This script assumes that the file paths for the images and styles CSV files are correctly set in the main section of the script.
+"""
+
 import pandas as pd
 
 def merge_datasets(images_csv_path, styles_csv_path, output_csv_path):
     """
     Merges two datasets: one containing image URLs and another containing styles information.
+
+    Args:
+        images_csv_path (str): The file path for the CSV file containing image URLs.
+        styles_csv_path (str): The file path for the CSV file containing styles information.
+        output_csv_path (str): The output file path for the merged dataset.
+
+    Returns:
+        None: The function saves the merged dataset to a CSV file specified by output_csv_path.
+
+    Notes:
+        The function modifies the 'filename' field in the images dataset by removing the '.jpg' extension and converts
+        the 'id' in the styles dataset from integer to string to facilitate the merging based on these modified fields.
+
+    Author: ``@Ehsan138``
     """
     # Load the datasets
     images_df = pd.read_csv(images_csv_path)
