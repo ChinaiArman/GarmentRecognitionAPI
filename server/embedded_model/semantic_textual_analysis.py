125e8f5c4800e98a65c3f4e625dc6aa71595176b
Ehsan138 <67409692+Ehsan138@users.noreply.github.com>
2024-05-13 11:02:28 -0700

    Complete implementation for average_pool function

--- server/embedded_model/semantic_textual_analysis.py
+++ server/embedded_model/semantic_textual_analysis.py
@@ -11,9 +11,9 @@ import pandas as pd
 
 def average_pool(last_hidden_states: Tensor, attention_mask: Tensor) -> Tensor:
     """
-    no idea what this does.
     """
-    pass
+    last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
+    return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
 
 
 def normalize_embeddings(embeddings: Tensor):
