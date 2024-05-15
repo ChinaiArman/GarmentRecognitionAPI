f11cbd9588db5b215b1009b39e4525368451ff65
Arman Chinai <chinaiarman@gmail.com>
2024-05-15 02:16:28 -0700

    Init garment_recognizer class

---
+++ server/garment_recognizer.py
@@ -0,0 +1,17 @@
+"""
+"""
+
+
+from data_source.data_access import Database
+from embedded_model.semantic_textual_analysis import model_wrapper, load_embedded_model
+
+
+class GarmentRecognizer:
+    """
+    """
+    def __init__(
+        self
+    ) -> None:
+        """
+        """
+        pass
