b915551a69a838789347c5a32aea3da46b22e4a2
Collin Chan <omiyage4949@gmail.com>
2024-05-27 16:29:43 -0700

    Add search bar

--- ui/static/swagger-custom.js
+++ ui/static/swagger-custom.js
@@ -10,3 +10,45 @@ const MyLogoPlugin = function (system) {
     },
   };
 };
+
+const AdvancedFilterPlugin = function (system) {
+  return {
+    fn: {
+      opsFilter: function (taggedOps, phrase) {
+        phrase = phrase.toLowerCase();
+        //first filter out all actions that don't meet the search criteria
+        var filteredActions = taggedOps.map((tagObj) => {
+          tagObj._root.entries[1][1] = tagObj._root.entries[1][1].filter(
+            (operationObj) => {
+              var op = JSON.parse(JSON.stringify(operationObj));
+              var summary = "";
+              var description = "";
+              if (typeof op.operation.summary !== "undefined") {
+                summary = JSON.stringify(op.operation.summary).toLowerCase();
+              }
+              if (typeof op.operation.description !== "undefined") {
+                description = JSON.stringify(
+                  op.operation.description
+                ).toLowerCase();
+              }
+              if (
+                op.path.toLowerCase().indexOf(phrase) === -1 &&
+                summary.indexOf(phrase) === -1 &&
+                description.indexOf(phrase) === -1
+              ) {
+                return false;
+              } else {
+                return true;
+              }
+            }
+          );
+          return tagObj;
+        });
+        //then filter any Tags with no actions remaining
+        return filteredActions.filter((tagObj) => {
+          return tagObj._root.entries[1][1].size > 0;
+        });
+      },
+    },
+  };
+};
